f8cc914cdcceb38452567e2aed3b21f6e970774a
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-10-02 14:03:04 +0300

    deleted new nav

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -6,7 +6,7 @@ import ContactForm from "../../ContactForm/ContactForm";
 import Footer from "../../Footer/Footer";
 import { BrowserRouter, Link } from "react-router-dom";
 import jeanPhoto from "../../../Assets/images/jean-profile.png";
-import Navbar from "../../NewNav/Navbar";
+
 
 function Home() {
   return (
