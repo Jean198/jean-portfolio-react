2ef71b4d6b0d1728bc84dd0caec3285954dcabc4
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-11-21 11:42:53 +0200

    dynamic letter of recommendation

--- src/Components/Recommendations/Recommendations.js
+++ src/Components/Recommendations/Recommendations.js
@@ -1,6 +1,9 @@
 import React from "react";
 import recommendations from "../../Assets/Recommendations.json";
 import "./Recommendations.scss";
+import softrain from "../../Assets/documents/softrain-letter.pdf";
+
+const recommendationLetters = [{ name:"softrain", file: softrain },{name:"name", file:"file"}];
 
 const Recommendations = () => {
   return (
@@ -10,7 +13,15 @@ const Recommendations = () => {
           <h4>{recommendation.name}</h4>
           <p>{recommendation.position}</p> <br />
           <p className="email">{recommendation.email}</p> <br />
-          <p>{recommendation.letter}</p>
+          <a
+            className="letter-link"
+            href={recommendationLetters.find(letter=>letter.name===recommendation.letterName)?.file}
+            alt="alt text"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {recommendationLetters.find(letter=>letter.name===recommendation.letterName)? "Letter Of Recommendation":null}
+          </a>
         </div>
       ))}
     </>
